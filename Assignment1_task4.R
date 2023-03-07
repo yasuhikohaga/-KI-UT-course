8d0e05187084c1e13e31f2d405b33e3d81c38b65
yasuhikohaga <72208087+yasuhikohaga@users.noreply.github.com>
2023-03-07 18:30:45 +0900

    Add files via upload

---
+++ Assignment1_task4.R
@@ -0,0 +1,23 @@
+#1. What is the square root of 10?
+sqrt(10)
+#The output is [1] 3.162278
+
+#2. What is the logarithm of 32 to the base 2?
+log(32,2)
+#The answer is [1] 5
+
+#3. What is the sum of the numbers from 1 to 1000?
+sum(1:1000)
+#The anser is [1] 500500
+
+#4. What is the sum of all even numbers from 2 to 1000?
+sum(seq(2, 1000, by = 2))
+#The answer is [1] 250500
+
+#5. How many pairwise comparisons are there for 100 genes?
+choose(n = 100, k = 2)
+#The ansewr is [1] 4950
+
+#6. And how many ways to arrange 100 genes in triples?
+choose(n = 100, k = 3)
+#The answer is [1] 161700
